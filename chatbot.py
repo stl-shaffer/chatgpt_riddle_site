2d3d0514a4785a5e6a77a54f952f38366e308b42
Scott Shaffer <106487373+stl-shaffer@users.noreply.github.com>
2023-07-04 13:31:25 -0400

    Update chatbot.py
    Update the file to use the OpenAI API

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, jsonify
-from ChatBotAPI import get_response # This is hypothetical
+import openai
+
+openai.api_key = 'your-api-key'
 
 app = Flask(__name__)
 
 @app.route('/get_response', methods=['POST'])
 def chatbot_response():
     user_text = request.json['message']
-    bot_response = get_response(user_text) # This is a hypothetical function, replace with actual function call to your API.
+
+    response = openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=user_text,
+      max_tokens=150
+    )
+
+    bot_response = response.choices[0].text.strip()
+
     return jsonify({'message': bot_response})
 
 if __name__ == '__main__':
